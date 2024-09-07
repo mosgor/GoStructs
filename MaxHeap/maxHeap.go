3a66aa12d5eac605dac0a13e1c5e3439cdeca6fe
Maxim Mikhailov <mos_gor@list.ru>
2024-09-08 02:39:17 +0300

    build: updated maxHeap

--- MaxHeap/maxHeap.go
+++ MaxHeap/maxHeap.go
@@ -1,65 +1,83 @@
 package maxHeap
 
+import "errors"
+
 type values interface {
-	~int | ~float32 |
-		~string | ~byte |
-		~rune
+	~int8 | ~int16 | ~int32 | ~int64 | ~int |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
+		~float64 | ~float32 |
+		~string
 }
 
 type MaxHeap[T values] []T
 
-func (h MaxHeap[T]) Insert(k T) MaxHeap[T] {
-	h = append(h, k)
-	for i := len(h) - 1; i != 0; {
-		if h[i] > h[(i-1)/2] {
-			h[i], h[(i-1)/2] = h[(i-1)/2], h[i]
+func (h *MaxHeap[T]) Insert(k T) error {
+	if h == nil {
+		return errors.New("heap pointer is nil")
+	}
+	*h = append(*h, k)
+	for i := len(*h) - 1; i != 0; {
+		if (*h)[i] > (*h)[(i-1)/2] {
+			(*h)[i], (*h)[(i-1)/2] = (*h)[(i-1)/2], (*h)[i]
 			i = (i - 1) / 2
 		} else {
 			break
 		}
 	}
-	return h
+	return nil
 }
 
-func (h MaxHeap[T]) ExtractMax() (MaxHeap[T], T) {
-	heapLen := len(h) - 1
-	if heapLen+1 == 0 {
+func (h *MaxHeap[T]) Extract() (T, bool) {
+	if h == nil || len(*h) == 0 {
 		var defValue T
-		return h, defValue
+		return defValue, false
 	}
-	res := h[0]
-	h[0] = h[heapLen]
-	h = h[:heapLen]
-	var ind int
+	heapLen := len(*h) - 1
+	res := (*h)[0]
+	(*h)[0] = (*h)[heapLen]
+	*h = (*h)[:heapLen]
+	siftDown(*h, 0)
+	return res, true
+}
+
+func (h *MaxHeap[T]) Max() (T, bool) {
+	if h == nil || len(*h) == 0 {
+		var defValue T
+		return defValue, false
+	}
+	return (*h)[0], true
+}
+
+func Heapify[T values](arr []T) MaxHeap[T] {
+	for i := len(arr)/2 - 1; i >= 0; i-- {
+		ind := i
+		siftDown(arr, ind)
+	}
+	return arr
+}
+
+func siftDown[T values](arr []T, ind int) {
+	heapLen := len(arr)
 	for {
 		if ind*2+1 >= heapLen {
 			break
 		}
 		if ind*2+2 >= heapLen {
-			if h[ind] < h[ind*2+1] {
-				h[ind], h[ind*2+1] = h[ind*2+1], h[ind]
+			if arr[ind] < arr[ind*2+1] {
+				arr[ind], arr[ind*2+1] = arr[ind*2+1], arr[ind]
 			}
 			break
 		}
-		left, right := h[ind*2+1], h[ind*2+2]
-		if h[ind] > left && h[ind] > right {
+		left, right := arr[ind*2+1], arr[ind*2+2]
+		if arr[ind] > left && arr[ind] > right {
 			break
 		}
 		if left > right {
-			h[ind], h[ind*2+1] = h[ind*2+1], h[ind]
+			arr[ind], arr[ind*2+1] = arr[ind*2+1], arr[ind]
 			ind = ind*2 + 1
 		} else {
-			h[ind], h[ind*2+2] = h[ind*2+2], h[ind]
+			arr[ind], arr[ind*2+2] = arr[ind*2+2], arr[ind]
 			ind = ind*2 + 2
 		}
 	}
-	return h, res
-}
-
-func (h MaxHeap[T]) Max() T {
-	if len(h) == 0 {
-		var defValue T
-		return defValue
-	}
-	return h[0]
 }
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # GoStructs
-Data structures for Go
+GoStructs - A library that provides basic data structures and their rare variations.
+
+There are such data structures: 
+1) [Heap](#heap)
+    - [MaxHeap](#maxheap)
+    - [MinHeap](#minheap)
+## Heap
+
+---
+Tree-based sorted data structure. Every operation takes about $O(log{n})$.  
+Every slice can be transformed to the Heap with $O(n)$ complexity.  
+It's based on slices, so such functions as len or cap are also **working** with it.
+
+### MaxHeap
+Heap sorted from the biggest number to the smallest.  
+There are such methods implemented in MaxHeap:
+- **Insert.** Inserts an element into a Heap. Takes this element as an argument. Returns error value.
+- **Extract.** Extracts the biggest element of the Heap and returns it and the true bool value.    
+When we try to extract from an empty Heap, we get the default value of a type contained in the Heap and the false bool value.
+- **Max.** Returns the biggest element of the Heap and the true bool value.   
+When this method is used on an empty Heap, we get the default value of a type contained in the Heap and the false bool value.
+
+There is only one function:
+- **Heapify.** It takes slice as an argument and returns new Heap consisting of slice elements.
+
+### MinHeap
